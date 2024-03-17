613c306683a4764794d8c11dc1e0dfcb46be02ce
a <44981148+1x6@users.noreply.github.com>
2024-03-17 10:30:18 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import pyzbar
+from pyzbar.pyzbar import decode
+from PIL import Image
+from flask import request, jsonify, Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    img = Image.open('barcode.png')
+    decoded_list = decode(img)
+    print(len(decoded_list))
+    print(decoded_list)
+    return str(decoded_list)
+
+if __name__ == '__main__':
+    #app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000)) 
